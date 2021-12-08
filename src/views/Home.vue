1de0becf93280b61c991a46c67e072583228f9d3
Olava Faksdal <olava.faksdal@gmail.com>
2021-12-08 10:44:50 +0100

    added vue router

---
+++ src/views/Home.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="home">
+    <!-- img alt="Vue logo" src="../assets/logo.png" /-->
+    <ThreeColumns />
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import ThreeColumns from "@/components/ThreeColumns.vue";
+
+export default {
+  name: "Home",
+  components: {
+    ThreeColumns,
+  },
+};
+</script>
