9cb63f7f27854c6a2f71361768a844b2ca216b2b
Olava Faksdal <38139899+olavis@users.noreply.github.com>
2023-02-14 13:40:13 +0100

    add about section

--- README.md
+++ README.md
@@ -1,4 +1,5 @@
 # hello-vue
+The Hello-vue app was created in 2021 as an attempt to learn Vue, by making an Hello World application. Unfortunately, it turned out to be just another cheesy dating app, and my (slim) experience with Vue still hasn't come up handy..
 
 ## Project setup
 ```
--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,6 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <h1>About Hello-vue</h1>
+    <p>The Hello-vue app was created in 2021 as an attempt to learn Vue, by making an Hello World application. Unfortunately, it turned out to be just another cheesy dating app, and my (slim) experience with Vue still hasn't come up handy..</p>
   </div>
 </template>
